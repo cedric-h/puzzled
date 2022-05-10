7ccd336707ba85873d45c61d832da9106fb65d01
Cedric Hutchings <ced@misguided.enterprises>
2022-05-10 00:43:46 -0400

    sokoban

--- index.js
+++ index.js
@@ -39,11 +39,8 @@ const art = Object.fromEntries(await Promise.all(
 
 const { sign, abs, round, floor, cos, sin, random } = Math;
 
-let map = {};
 const onMap = (x, y) => x < w && x >= 0 &&
                         y < h && y >= 0;
-const mapSet = (obj, x, y) =>
-  map[[floor(x), floor(y)]] = obj;
 const mapNeighbors = (x, y) => {
   return [
     [x-1, y  ],
@@ -52,17 +49,111 @@ const mapNeighbors = (x, y) => {
     [x  , y-1],
   ];
 }
-const mapPos = obj => {
-  for (const pos in map)
-    if (map[pos] == obj)
-      return pos.split(",").map(x => +x);
+const makeMap = () => ({
+  data: {},
+  set(obj, x, y) {
+    this.data[[floor(x), floor(y)]] = obj;
+  },
+  pos(obj) {
+    for (const pos in this.data)
+      if (this.data[pos] == obj)
+        return pos.split(",").map(x => +x);
+  },
+  move(obj, nx, ny) {
+    for (const pos in this.data)
+      if (this.data[pos] == obj)
+        delete this.data[pos];
+    this.set(obj, nx, ny);
+  }
+})
+
+let map, goalMap;
+let player = { art: art.player };
+
+let level = 0;
+const levels = [
+`
+
+
+
+      wwwwwwwww
+      w    ooow
+      w w wowow
+      w w  ooow
+      w xxx w w
+      w xpx   w
+      w xxxww w
+      w       w
+      wwwwwwwww
+`,
+`
+
+
+     wwwwwwwwwww
+     w    p    w
+     w x x x x w
+     w  x x x  w
+     w w x x w w
+     w w  x  w w
+     w ww o ww w
+     w w o o w w
+     wwwo o owww
+      wo o o ow
+      wwwwwwwww
+`,
+];
+
+const loadLevel = () => {
+  map = makeMap();
+  goalMap = makeMap();
+
+  const splentries = (t, s) => Object.entries(t.split(s));
+  for (const [y, row] of splentries(levels[level], '\n'))
+    for (const [x, letter] of splentries(row, '')) {
+           if (letter == ' ') ;
+      else if (letter == 'p') map.set(player, x, y);
+      else if (letter == 'o') goalMap.set({ art: art.box }, x, y);
+      else
+        map.set({ art: log(({
+          'w': art.wall, 
+          'x': art.crate,
+        })[letter]) }, x, y);
+    }
 }
-const mapMove = (obj, nx, ny) => {
-  for (const pos in map)
-    if (map[pos] == obj)
-      delete map[pos];
-  mapSet(obj, nx, ny);
-}
+loadLevel();
+
+window.onkeydown = ev => {
+  let dx = 0, dy = 0;
+  if (ev.key == "w") dy--;
+  if (ev.key == "s") dy++;
+  if (ev.key == "a") dx--;
+  if (ev.key == "d") dx++;
+  if (ev.key == "r") { return loadLevel() };
+
+  let n = 0;
+  (function push(tile) {
+    if (n > 1) return false;
+    n++;
+
+    let [x, y] = map.pos(tile);
+    x += dx, y += dy;
+
+    const intheway = map.data[[x, y]];
+    if (intheway) {
+      if (intheway.art == art.wall || !push(map.data[[x, y]], x+dx, y+dy))
+        return false;
+    }
+    map.move(tile, x, y);
+    return true;
+  })(player);
+  
+  if (Object.keys(goalMap.data).every(pos => 
+    map.data[pos]?.art == art.crate &&
+        goalMap.data[pos]?.art == art.box
+  ))
+    level++,
+    loadLevel();
+};
 
 /* rot should be 0..3 */
 const drawTile = (art, x, y, rot=0, alpha=1) => {
@@ -80,10 +171,14 @@ requestAnimationFrame(function frame(now) {
   ctx.fillStyle = "rgb(47, 43, 49)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  for (const pos in map) {
-    const [x, y] = pos.split(",").map(x => +x);
-    drawTile(map[pos].art, x, y);
-  }
+  const drawMap = (map, alpha) => {
+    for (const pos in map.data) {
+      const [x, y] = pos.split(",").map(x => +x);
+      drawTile(map.data[pos].art, x, y, 0, alpha);
+    }
+  };
+  drawMap(map);
+  drawMap(goalMap, 0.5);
 
   requestAnimationFrame(frame);
 });
